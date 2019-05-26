b4de438bd4e442a8933ff3faa060c571a9d7f357
jose uc <arturouc1997@gmail.com>
2019-05-26 01:56:04 -0500

    Versión 1.0 del Backend

---
+++ config/db.php
@@ -0,0 +1,50 @@
+<?php
+
+class DB{
+  private $dbHost="localhost";
+  private $dbUsername="root";
+  private $dbPassword="root";
+  private $dbName="coox";
+
+  public $db_con;
+
+  public function __construct(){
+    if(!isset($this->db)){
+      try{
+        $pdo=new PDO("mysql:host=".$this->dbHost.";dbname=".$this->dbName, $this->dbUsername, $this->dbPassword);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->db_con=$pdo;
+      }catch(PDOEXCEPTION $e){
+        die("Failed to connect with mysql :".$e->getMessage());
+      
+      }
+    }
+  }
+
+  public function userLogin($username, $password)
+  {
+    $stmt=$this->db_con->prepare("SELECT * FROM usuarios WHERE usuario=:username");
+    $stmt->execute(array(":username"=>$username));
+    $row=$stmt->fetch(PDO::FETCH_ASSOC);
+    $count=$stmt->rowCount();
+    if($row['contrasena']==$password){
+      return $count;
+    }else{
+      return false;
+    }
+  }
+
+  public function registerUser($username, $userpassword, $useremail, $usercontact, $userregdate)
+  {
+    $sql="INSERT into usuarios(usuario, contrasena, correo, usercontact, userregdate) VALUES (:username, :userpassword, :useremail, :usercontact, :userregdate)";
+    $stmt=$this->db_con->prepare($sql);
+    $stmt->bindParam(":username", $username);
+    $stmt->bindParam(":userpassword", $userpassword);
+    $stmt->bindParam(":useremail", $useremail);
+    $stmt->bindParam(":usercontact", $usercontact);
+    $stmt->bindParam(":userregdate", $userregdate);
+    $stmt->execute();
+    $newId=$this->db_con->lastInsertId();
+    return $newId;
+  }
+}
